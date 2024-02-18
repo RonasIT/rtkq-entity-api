f96c08f904c15ef8bdba2f0db709d0c7e3cb305f
Ilya Pakhomov <ipakhomov@ronasit.com>
2024-02-18 18:28:55 +0600

    chore: correct library utility export

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,3 +3,4 @@ export * from './create-api-creator';
 export * from './create-entity-api-utils';
 export * from './create-entity-instance';
 export * from './prepare-request-params';
+export * from './class-transformer';
