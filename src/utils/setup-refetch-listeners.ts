a10cc329b414761e25c9744b5625b7776e976b75
ekazankova <ekazankova@ronasit.com>
2024-09-18 20:29:06 +0600

    feat: added setupRefetchListeners
    Refs: https://app.clickup.com/t/24336023/PRD-1221

---
+++ src/utils/setup-refetch-listeners.ts
@@ -0,0 +1,55 @@
+import NetInfo, { NetInfoState } from '@react-native-community/netinfo';
+import { Dispatch } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/query';
+import { AppState, Platform } from 'react-native';
+
+export function setupRefetchListeners(
+  storeDispatch: Dispatch,
+  options: { refetchOnReconnect?: boolean; refetchOnFocus?: boolean } = {
+    refetchOnReconnect: true,
+    refetchOnFocus: true
+  },
+): () => void {
+  return setupListeners(storeDispatch, (dispatch, actions) => {
+    if (!Platform?.OS || Platform.OS === 'web') {
+      throw new Error(
+        'The \'setupRefetchListeners\' utility is intended for use with React Native. For web please use \'setupListeners\' from  @reduxjs/toolkit/query.',
+      );
+    }
+
+    if (!NetInfo?.addEventListener) {
+      throw new Error(
+        'To use \'refetchOnReconnect\' and \'refetchOnFocus\' you must setup @react-native-community/netinfo package.',
+      );
+    }
+
+    const unsubscribeHandlers: Array<() => void> = [];
+
+    if (options.refetchOnFocus) {
+      const { remove: appStateUnsubscribe } = AppState.addEventListener('change', async (state) => {
+        const { isConnected, isInternetReachable } = await NetInfo.fetch();
+        const hasConnection = isConnected && isInternetReachable;
+
+        if (hasConnection) {
+          dispatch(state === 'active' ? actions.onFocus() : actions.onFocusLost());
+        }
+      });
+
+      unsubscribeHandlers.push(appStateUnsubscribe);
+    }
+
+    if (options.refetchOnReconnect) {
+      const netInfoUnsubscribe = NetInfo.addEventListener(({ isConnected, isInternetReachable }: NetInfoState) => {
+        const hasConnection = !!(isConnected && isInternetReachable);
+
+        dispatch(hasConnection ? actions.onOnline() : actions.onOffline());
+      });
+
+      unsubscribeHandlers.push(netInfoUnsubscribe);
+    }
+
+    return (): void => {
+      unsubscribeHandlers.forEach((handleUnsubscribe) => handleUnsubscribe?.());
+    };
+  });
+}
